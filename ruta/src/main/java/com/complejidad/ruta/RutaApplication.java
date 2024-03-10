d5ebce5720857e7457d8e35a30c0aed1150850bb
Ricardo Villegas <kevin.ricardo@ciencias.unam.mx>
2024-03-10 15:48:19 -0600

    3SAT

---
+++ ruta/src/main/java/com/complejidad/ruta/RutaApplication.java
@@ -0,0 +1,8 @@
+package com.complejidad.ruta;
+
+public class RutaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
+}
---
+++ ruta/src/test/java/com/complejidad/ruta/RutaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complejidad.ruta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
