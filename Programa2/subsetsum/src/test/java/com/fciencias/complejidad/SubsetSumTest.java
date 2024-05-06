29d468e00ee120e7b919837b0991a09ac0668a55
Ricardo Villegas <kevin.ricardo@ciencias.unam.mx>
2024-05-06 12:08:26 -0600

    Programa 2

--- Programa2/subsetsum/src/test/java/com/fciencias/complejidad/SubsetSumTest.java
+++ Programa2/subsetsum/src/test/java/com/fciencias/complejidad/SubsetSumTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SubsetSum {
+class SubsetSumTest {
 
 	@Test
 	void contextLoads() {
